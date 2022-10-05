743d8b4eaca807de735e73b4588f7e35a5c0987b
JeongTaek Oh <tei1234tei@gmail.com>
2022-10-05 22:05:16 +0900

    delete annotated sqlalchemy ddl emitting call

--- app/main.py
+++ app/main.py
@@ -5,10 +5,8 @@ from .database import engine
 
 myApp = FastAPI()
 
-# simple database migrations if there isn't migration tools
-# models.Base.metadata.create_all(bind=engine)
-
 myApp.include_router(api.api_router)
+
 
 @myApp.get("/")
 async def get_root():
