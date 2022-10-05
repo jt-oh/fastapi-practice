cab2d6adc6bcce0afd77991d75566e6b39f1302d
JeongTaek Oh <tei1234tei@gmail.com>
2022-10-05 22:03:56 +0900

    extract test data and make user create test passed

--- app/tests/api/v1/endpoints/test_users.py
+++ app/tests/api/v1/endpoints/test_users.py
@@ -1,19 +1,23 @@
 from fastapi.testclient import TestClient
 
-def test_create_user(client: TestClient):
-    age = 25
-    email = "random_user@nomail.com"
-    password = "rondompwd"
-    json_data = {"age": age, "email": email, "password": password}
+test_user_data = [
+    {"email": "tom@nomail.com", "password": "tompwd", "age": 28},
+    {"email": "tina@nomail.com", "password": "tinapwd", "age": 38},
+    {"email": "jack@nomail.com", "password": "jackpwd", "age": 17},
+    {"email": "sandy@nomail.com", "password": "sandypwd", "age": 21},
+    {"email": "brian@nomail.com", "password": "brianpwd", "age": 50},
+    {"email": "kelly@nomail.com", "password": "kellypwd", "age": 35}
+]
 
-    r = client.post("/users/", json=json_data)
-    print(r)
+def test_create_user(client: TestClient, configure_test_db):
+    user_data = test_user_data[0]
+
+    r = client.post("/users/", json=user_data)
     res = r.json()
-    print(res)
 
     assert(r.status_code is 200)
-    assert(res.age is age)
-    assert(res.email is email)
+    assert(res["age"] is user_data["age"])
+    assert(res["email"] == user_data["email"])
     assert("password" not in res)
 
 # def test_get
